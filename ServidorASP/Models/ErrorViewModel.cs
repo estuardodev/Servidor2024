99cd93b747204e17c408ff6e01785ac943bd3fdc
Estuardo Ramírez <estuardito_9@hotmail.com>
2024-02-22 16:50:18 -0600

    Se creó el proyecto de ASP.Net Core 8.0

---
+++ ServidorASP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ServidorASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
