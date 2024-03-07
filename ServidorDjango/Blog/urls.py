b2b9311643995f30bf03b2e54de5f09b83bba2c7
Estuardo Ramírez <estuardito_9@hotmail.com>
2024-03-07 17:12:24 -0600

    Ahora la api puede servir recursos static de wwwroot

--- ServidorDjango/Blog/urls.py
+++ ServidorDjango/Blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('getImage', views.getImage, name='getImage'),
+    path('getStatic', views.getStatic, name='getStatic'),
 ]
--- ServidorDjango/Blog/views.py
+++ ServidorDjango/Blog/views.py
@@ -6,6 +6,7 @@ from io import BytesIO
 from PIL import Image
 
 # Create your views here.
+STATIC_DIR = os.path.join(settings.SHARE_DIR, 'ServidorASP', 'wwwroot')
 MEDIA_ROOT = settings.MEDIA_ROOT
 
 def getImage(request):
@@ -17,6 +18,37 @@ def getImage(request):
         return HttpResponseBadRequest('No se ha especificado una imagen')
     
     image_path = f'{MEDIA_ROOT}/{image_file}'
+    if not os.path.exists(image_path):
+        print(image_path)
+        return HttpResponseBadRequest('La imagen no existe')
+    
+    image = Image.open(image_path)
+    if size:
+        size = int(size)
+        image.thumbnail((size, size))
+
+    if format:
+        format = format.lower()
+        if format not in ('jpeg', 'png', 'webp'):
+            return HttpResponseBadRequest('Formato no válido')
+        
+        image_format = BytesIO()
+        image.save(image_format, format=format.upper())
+        return HttpResponse(image_format.getvalue(), content_type=f'image/{format}')
+    
+    image_io = BytesIO()
+    image.save(image_io, format=image.format)
+    return HttpResponse(image_io.getvalue(), content_type=f'image/{image.format.lower()}')
+
+def getStatic(request):
+    image_file = request.GET.get('image')
+    size = request.GET.get('size')
+    format = request.GET.get('format')
+
+    if not image_file:
+        return HttpResponseBadRequest('No se ha especificado una imagen')
+    
+    image_path = f'{STATIC_DIR}/{image_file}'
     if not os.path.exists(image_path):
         print(image_path)
         return HttpResponseBadRequest('La imagen no existe')
