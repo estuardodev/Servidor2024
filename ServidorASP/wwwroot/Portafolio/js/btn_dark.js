bc5736ab14a8c921d986b1a3b7750922e2acc724
Estuardo Ramírez <estuardito_9@hotmail.com>
2024-03-09 23:08:38 -0600

    Se implemento el detectar modo oscuro al modo oscuro

--- ServidorASP/wwwroot/Portafolio/js/btn_dark.js
+++ ServidorASP/wwwroot/Portafolio/js/btn_dark.js
@@ -1,14 +1,22 @@
 ﻿document.addEventListener('DOMContentLoaded', function () {
     var themeToggleBtn = document.getElementById('theme-toggle');
 
-  
+    // Obtener la preferencia de tema del usuario
+    var prefersDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
     var existClave = localStorage.getItem('dark_mode');
     console.log(existClave);
 
     if (existClave === 'true') {
         // Obtener el estado del modo oscuro desde localStorage
         document.documentElement.classList.add('dark');
-    } 
+    } else {
+        var useDarkMode = prefersDarkMode;
+        // Establecer el estado inicial del interruptor de tema y el tema
+        themeToggleBtn.checked = useDarkMode;
+        document.documentElement.classList.toggle('dark', useDarkMode);
+    }
+
 
     themeToggleBtn.addEventListener('change', function () {
         // Cambiar el tema al hacer clic en el interruptor
