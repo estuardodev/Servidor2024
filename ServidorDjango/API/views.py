a09059cd40c94545c09ecbcc1c57b6676490a16b
Estuardo Ramírez <estuardito_9@hotmail.com>
2024-03-26 14:48:04 -0600

    Se agregó una authorization berarer para el acceder al api

--- ServidorDjango/API/views.py
+++ ServidorDjango/API/views.py
@@ -6,51 +6,58 @@ from django.template.loader import render_to_string
 from weasyprint import HTML
 from weasyprint.document import FontConfiguration
 from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
 
 # Create your views here.
+authorization = settings.AUTHORIZATION
 
 @csrf_exempt
 def export_pdf(request):
     context = {}
-    if request.body and request.method == "POST":
-        try:
-            body = request.body
-            data = json.loads(body)
-            key_to_remove:list = []
-            for key in data:
-                if data[key] == None:
-                    key_to_remove.append(key)
-            
-            for key in key_to_remove:
-                del data[key]
+    authorization_header = request.headers.get('Authorization')
+    if authorization_header == authorization:
+                  
+        if request.body and request.method == "POST":
+            try:
+                body = request.body
+                data = json.loads(body)
+                data = {key: value for key, value in data.items() if value is not None}
 
-            persepction:list = [value for value in data["extraPerceptions"] if value is not None]
-            data["extraPerceptions"] = persepction
+                persepction:list = [value for value in data["extraPerceptions"] if value is not None]
+                data["extraPerceptions"] = persepction
 
-            persepction:list = [value for value in data["extraDeductions"] if value is not None]
-            data["extraDeductions"] = persepction
+                persepction:list = [value for value in data["extraDeductions"] if value is not None]
+                data["extraDeductions"] = persepction
 
-            context["data"] = data
+                context["data"] = data
 
-            persepctions:list = data["perceptions"]
-            persepctions = persepctions.split(",")
-            bonos:list = [i.split("$") for i in persepctions]
-            context["perceptions"] = bonos
-        except Exception as e:
-            print(e)
+                persepctions:list = data["perceptions"].split(",")
+                bonos:list = [i.split("$") for i in persepctions]
+                data["perceptions"] = bonos
+            except Exception as e:
+                print(e)
 
-        hora_actual = datetime.datetime.now()
+            hora_actual = datetime.datetime.now()
 
-        hora_am_pm = hora_actual.strftime("%I:%M %p")
-        context['hour_am_pm'] = hora_am_pm
+            hora_am_pm = hora_actual.strftime("%I:%M %p")
+            context['hour_am_pm'] = hora_am_pm
 
-        html = render_to_string("app/report.html", context)
+            html = render_to_string("app/report.html", context)
+            response = HttpResponse(content_type="application/pdf")
+            response["Content-Disposition"] = "inline; report.pdf"
+            font_config = FontConfiguration()
+            HTML(string=html, base_url=request.build_absolute_uri()).write_pdf(response, font_config=font_config)
+
+            return response
+        else:
+            return HttpResponseBadRequest("No data found")
+    else:
+        html = '''<h1>Authorization failed</h1>'''
         response = HttpResponse(content_type="application/pdf")
         response["Content-Disposition"] = "inline; report.pdf"
         font_config = FontConfiguration()
         HTML(string=html, base_url=request.build_absolute_uri()).write_pdf(response, font_config=font_config)
 
-        return response
-    else:
-        return HttpResponseBadRequest("No data found")
+        return HttpResponseBadRequest(response)
+
 
--- ServidorDjango/ServidorDjango/settings.py
+++ ServidorDjango/ServidorDjango/settings.py
@@ -35,6 +35,8 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
+AUTHORIZATION = env('AUTHORIZATION')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
