436fdfe4d1166b428d6a66dfad62c8dfb422a08d
Estuardo Ramírez <estuardito_9@hotmail.com>
2024-03-26 20:56:14 -0600

    Se optimizó el API de export_pdf y se arregló un bug con el authorization

--- ServidorDjango/API/views.py
+++ ServidorDjango/API/views.py
@@ -12,56 +12,42 @@ authorization = settings.AUTHORIZATION
 
 @csrf_exempt
 def export_pdf(request):
+    if request.headers.get("Authorization") != authorization:
+        return HttpResponseBadRequest("Unauthorized")
+    
+    if not request.body and request.method != "POST":
+        return HttpResponseBadRequest("No data found")
+    
     context = {}
-    #authorization_header = request.headers.get('Authorization')
-    #if authorization_header == authorization:
-                  
-    if request.body and request.method == "POST":
-        try:
-            body = request.body
-            data = json.loads(body)
-            data = {key: value for key, value in data.items() if value is not None}
+    try:
+        body = request.body
+        data = json.loads(body)
+        data = {key: value for key, value in data.items() if value is not None}
 
-            persepction:list = [value for value in data["extraPerceptions"] if value is not None]
-            data["extraPerceptions"] = persepction
+        persepction:list = [value for value in data["extraPerceptions"] if value is not None]
+        data["extraPerceptions"] = persepction
 
-            persepction:list = [value for value in data["extraDeductions"] if value is not None]
-            data["extraDeductions"] = persepction
-            context["data"] = data
+        persepction:list = [value for value in data["extraDeductions"] if value is not None]
+        data["extraDeductions"] = persepction
+        context["data"] = data
+    
+        persepctions:list = data["perceptions"]
+        persepctions:list = persepctions.split(",")
+        bonos:list = [i.split("$") for i in persepctions]
+        data["perceptions"] = bonos
+    except Exception as e:
+        print(e)
 
-            persepctions:list = data["perceptions"].split(",")
-            bonos:list = [i.split("$") for i in persepctions]
-            data["perceptions"] = bonos
-        except Exception as e:
-            print(e)
+    hora_actual = datetime.datetime.now()
 
-            hora_actual = datetime.datetime.now()
+    hora_am_pm = hora_actual.strftime("%I:%M %p")
+    context['hour_am_pm'] = hora_am_pm
 
-            hora_am_pm = hora_actual.strftime("%I:%M %p")
-            context['hour_am_pm'] = hora_am_pm
-
-            html = render_to_string("app/report.html", context)
-            response = HttpResponse(content_type="application/pdf")
-            response["Content-Disposition"] = "inline; report.pdf"
-            font_config = FontConfiguration()
-            HTML(string=html, base_url=request.build_absolute_uri()).write_pdf(response, font_config=font_config)
-
-        return response
-    else:
-        html = '''<h1>No data found</h1>'''
-        response = HttpResponse(content_type="application/pdf")
-        response["Content-Disposition"] = "inline; report.pdf"
-        font_config = FontConfiguration()
-        HTML(string=html, base_url=request.build_absolute_uri()).write_pdf(response, font_config=font_config)
-
-        return HttpResponseBadRequest(response)
-'''    else:
-        html = <h1>Authorization failed</h1>
-        response = HttpResponse(content_type="application/pdf")
-        response["Content-Disposition"] = "inline; report.pdf"
-        font_config = FontConfiguration()
-        HTML(string=html, base_url=request.build_absolute_uri()).write_pdf(response, font_config=font_config)
-
-        return HttpResponseBadRequest(response)
-'''
+    html = render_to_string("app/report.html", context)
+    response = HttpResponse(content_type="application/pdf")
+    response["Content-Disposition"] = "inline; report.pdf"
+    font_config = FontConfiguration()
+    HTML(string=html, base_url=request.build_absolute_uri()).write_pdf(response, font_config=font_config)
+    return response
+   
 
