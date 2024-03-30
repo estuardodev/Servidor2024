151bb5e09df1db3e10ffe53a2a0ce967fea0de4e
Estuardo Ramírez <estuardito_9@hotmail.com>
2024-03-30 16:18:52 -0600

    Se mejoró el rendimiento de la generación de los pdf y se estructuró mejor

--- ServidorDjango/API/views.py
+++ ServidorDjango/API/views.py
@@ -1,66 +1,52 @@
 import json
-import datetime
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.template.loader import render_to_string
 from weasyprint import HTML
 from weasyprint.document import FontConfiguration
 from django.views.decorators.csrf import csrf_exempt
-from django.conf import settings
 
 # Create your views here.
+def ConvertToListDict(data) -> list:
+    datos = []
+    for index in range(len(data)):
+        dic = {}
+        for key, value in data[index].items():
+            dic[key] = value
+        datos.append(dic)
+    return datos
 
 @csrf_exempt
 def export_pdf(request):
     context = {}
+    if request.method != "POST" or not request.body:
+        return HttpResponseBadRequest("Bad Request")
 
-    if request.body and request.method == "POST":
-        try:
-            body = request.body
-            data = json.loads(body)
+    try:
+        body = request.body
+        data = json.loads(body)
+        data = ConvertToListDict(data)
             
-            datos = []
-            for index in range(len(data)):
-                dic = {}
-                for key, value in data[index].items():
-                    dic[key] = value
-                datos.append(dic)
+        for index in range(len(data)):
+            extraPerceptions:list = [value for value in data[index]["extraPerceptions"] if value is not None and value != ""]
+            data[index]["extraPerceptions"] = extraPerceptions
 
-            data = datos
+            extraDeductions:list = [value for value in data[index]["extraDeductions"] if value is not None and value != ""]
+            data[index]["extraDeductions"] = extraDeductions
 
-            
-            for index in range(len(data)):
-                extraPerceptions:list = [value for value in data[index]["extraPerceptions"] if value is not None]
-                data[index]["extraPerceptions"] = extraPerceptions
-
-                extraDeductions:list = [value for value in data[index]["extraDeductions"] if value is not None]
-                data[index]["extraDeductions"] = extraDeductions
-
-                persepctions:list = data[index]["perceptions"].split(",")
-                bonos:list = [i.split("$") for i in persepctions]
-                data[index]["perceptions"] = bonos
+            persepctions:list = data[index]["perceptions"].split(",")
+            bonos:list = [i.split("$") for i in persepctions]
+            data[index]["perceptions"] = bonos
 
 
-            context["datas"] = data
+        context["datas"] = data
 
-            html = render_to_string("app/report.html", context)
-            response = HttpResponse(content_type="application/pdf")
-            response["Content-Disposition"] = "inline; report.pdf"
-            font_config = FontConfiguration()
-            HTML(string=html, base_url=request.build_absolute_uri()).write_pdf(response, font_config=font_config)
+        html = render_to_string("app/report.html", context)
+        response = HttpResponse(content_type="application/pdf")
+        response["Content-Disposition"] = "inline; report.pdf"
+        font_config = FontConfiguration()
+        HTML(string=html, base_url=request.build_absolute_uri()).write_pdf(response, font_config=font_config)
 
-            return response
-        except Exception as e:
-            print(e)
-            return HttpResponseBadRequest("Error")
-        
-
-
-#data = {key: value for key, value in data.items() if value is not None}
-
-            
-
-            
-
-            #context["data"] = data
-
-            
+        return response
+    except Exception as e:
+        print(e)
+        return HttpResponseBadRequest("Bad Request")
