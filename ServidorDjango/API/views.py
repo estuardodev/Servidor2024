f8ce4c19ae345683f5ec8591786be11606d008e1
Estuardo Ramírez <estuardito_9@hotmail.com>
2024-03-30 21:54:08 -0600

    Se mejoró el api del pdf

--- ServidorDjango/API/views.py
+++ ServidorDjango/API/views.py
@@ -1,5 +1,5 @@
 import json
-from django.http import HttpResponse, HttpResponseBadRequest
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseServerError
 from django.template.loader import render_to_string
 from weasyprint import HTML
 from weasyprint.document import FontConfiguration
@@ -17,46 +17,44 @@ def ConvertToListDict(data) -> list:
 
 @csrf_exempt
 def export_pdf(request):
-    context = {}
+    context:dict = {}
     if request.method != "POST" or not request.body:
         return HttpResponseBadRequest("Bad Request")
 
     try:
         body = request.body
         data = json.loads(body)
-        data = ConvertToListDict(data)
-            
-
-        print(data[1]["kitchenDeduction"])
+        data:list = ConvertToListDict(data)
+        
         for index in range(len(data)):
+            if "extraPerceptions" in data[index]:
+                if data[index]["extraPerceptions"] != None:
+                    extraPerceptions:list = [value for value in data[index]["extraPerceptions"] if value is not None and value != ""]
+                    data[index]["extraPerceptions"] = extraPerceptions
 
-            if data[index]["extraPerceptions"] != None:
-                extraPerceptions:list = [value for value in data[index]["extraPerceptions"] if value is not None and value != ""]
-                data[index]["extraPerceptions"] = extraPerceptions
+            if "extraDeductions" in data[index]:
+                if data[index]["extraDeductions"] != None:
+                    extraDeductions:list = [value for value in data[index]["extraDeductions"] if value is not None and value != ""]
+                    data[index]["extraDeductions"] = extraDeductions
 
-            if data[index]["extraPerceptions"] != None:
-                extraDeductions:list = [value for value in data[index]["extraDeductions"] if value is not None and value != ""]
-                data[index]["extraDeductions"] = extraDeductions
-
-            if data[index]["kitchenDeduction"] != None:
-                kitchenDeduction:list = [value for value in data[index]["kitchenDeduction"] if value is not None and value != ""]
-                data[index]["kitchenDeduction"] = kitchenDeduction
+            if "kitchenDeduction" in data[index]:
+                if data[index]["kitchenDeduction"] != None:
+                    kitchenDeduction:list = [value for value in data[index]["kitchenDeduction"] if value is not None and value != ""]
+                    data[index]["kitchenDeduction"] = kitchenDeduction
             
-            persepctions:list = data[index]["perceptions"].split(",")
-            bonos:list = [i.split("$") for i in persepctions]
-            data[index]["perceptions"] = bonos
-
+            if "perceptions" in data[index]:
+                persepctions:list = data[index]["perceptions"].split(",")
+                bonos:list = [i.split("$") for i in persepctions]
+                data[index]["perceptions"] = bonos
     
-
         context["datas"] = data
-
-        html = render_to_string("app/report.html", context)
+        html:str = render_to_string("app/report.html", context)
         response = HttpResponse(content_type="application/pdf")
         response["Content-Disposition"] = "inline; report.pdf"
         font_config = FontConfiguration()
         HTML(string=html, base_url=request.build_absolute_uri()).write_pdf(response, font_config=font_config)
 
         return response
-    except Exception as e:
-        print(e)
-        return HttpResponseBadRequest("Bad Request")
+    except KeyError as e:
+        template_name = render_to_string("app/error.html")
+        return HttpResponseServerError(template_name)
