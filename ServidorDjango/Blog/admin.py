76e0e0e959aff587293e8ab6db06e77c1e961747
Estuardo Ramírez <estuardito_9@hotmail.com>
2024-06-02 18:53:45 -0600

    Se eliminó el blog antigüo, se comenzará a trabajar en el nuevo

--- ServidorDjango/Blog/admin.py
+++ ServidorDjango/Blog/admin.py
@@ -2,38 +2,3 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-class ArticuloAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'fecha_hora_creacion', 'estado')
-    search_fields = ('titulo', 'fecha_hora_creacion', 'autor', 'visitas') 
-    readonly_fields = ('visitas', 'likes',)
-    fields = ('titulo', 'visitas', 'likes', 'descripcion', 'tags', 'url', 'prioridad', 'contenido', 'image', 'descripcion_imagen', 'autor', 'estado', 'categorias')
-
-    actions = ['desactivar_todo']
-
-    def desactivar_todo(self, request, queryset):
-        estado_desactivado = models.EstadoArticulos.objects.get(id=2) 
-        for articulo in queryset:
-            articulo.estado = estado_desactivado
-            articulo.save()
-        
-    desactivar_todo.short_description = "Desactivar todos los articulos seleccionados"
-
-admin.site.register(models.Articulo, ArticuloAdmin)
-
-class CategoriaAdmin(admin.ModelAdmin):
-    list_display = ('nombre',)
-    search_fields = ('nombre',)
-
-admin.site.register(models.Categoria, CategoriaAdmin)
-
-class EstadoArticulosAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'estado')
-    search_fields = ('estado',)
-
-admin.site.register(models.EstadoArticulos, EstadoArticulosAdmin)
-
-class AutoresAdmin(admin.ModelAdmin):
-    list_display = ('nombre_autor', )
-    search_fields = ('nombre_autor',)
-
-admin.site.register(models.Autores, AutoresAdmin)
